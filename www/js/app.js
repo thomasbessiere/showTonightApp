bbdd817231057922ec540b5d2311e80a555c9363
thomasbessiere <thomas.bessiere@polytechnique.edu>
2016-10-11 14:49:37 +0200

    application statique

--- www/js/app.js
+++ www/js/app.js
@@ -40,44 +40,35 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
+  
+          .state('tab.dash', {
+            url: '/dash',
+            views: {
+              'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+              }
+            }
+          })
+          .state('tab.shows', {
+              url: '/shows',
+              views: {
+                'tab-shows': {
+                  templateUrl: 'templates/tab-shows.html',
+                  controller: 'ShowsCtrl'
+                }
+              }
+            })
+            .state('tab.show-detail', {
+              url: '/shows/:showId',
+              views: {
+                'tab-shows': {
+                  templateUrl: 'templates/show-detail.html',
+                  controller: 'ShowDetailCtrl'
+                }
+              }
+          });
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
--- www/js/services.js
+++ www/js/services.js
@@ -1,47 +1,53 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Shows', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+  var shows = [
+    {
+      id: 1,
+      name: "Mon premier Show",
+      location: "Salle Pleyel",
+      description: "Concert blabla",
+      capacity: 500,
+      price: 30,
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Salle_Pleyel_5.jpg/220px-Salle_Pleyel_5.jpg",
+      date: "2016-10-30",
+      url: "https://api-shows-tonight.herokuapp.com/shows/1.json"
+    },
+    {
+      id: 2,
+      name: "Sébastien Tellier",
+      location: "Nouveau Casino",
+      description: "L'Aventura",
+      capacity: 500,
+      price: 36,
+      image: "http://www.gqmagazine.fr/uploads/images/201421/cc/l_aventura_de_s__bastien_tellier_7651.jpeg",
+      date: "2016-10-19",
+      url: "https://api-shows-tonight.herokuapp.com/shows/2.json"
+    },
+    {
+      id: 5,
+      name: "Bertrand",
+      location: "Olympia Bruno Coquatrix",
+      description: "Concert de guitare classique",
+      capacity: 10,
+      price: 400,
+      image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Bob-Marley-in-Concert_Zurich_05-30-80.jpg",
+      date: "2016-11-05",
+      url: "https://api-shows-tonight.herokuapp.com/shows/5.json"
+    }
+  ];
 
   return {
     all: function() {
-      return chats;
+      return shows;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(showId) {
+      for (var i = 0; i < shows.length; i++) {
+        if (shows[i].id === parseInt(showId)) {
+          return shows[i];
         }
       }
       return null;
